af5e9919c8545ed02580e50a68968ad0dc335715
Arsientii Stupak <haskeryt322009@gmail.com>
2026-03-30 11:47:51 +0200

    Vertical scrollbar

--- script.js
+++ script.js
@@ -1,48 +1,55 @@
-const createTask = document.querySelector("#button-add-task");
-const input = document.querySelector("#input-task");
-const list = document.querySelector("#tasks");
+    const createTask = document.querySelector("#button-add-task");
+    const input = document.querySelector("#input-task");
+    const list = document.querySelector("#tasks");
 
-const noTaskMessage = document.createElement("p");
-noTaskMessage.textContent = "no tasks yet";
-noTaskMessage.id = "no-task-message";
-noTaskMessage.style.color = "#abb9bd";
-
-function updateNoTaskMessage() {
-    const msg = document.querySelector("#no-task-message");
-    if (list.children.length === 0 && !msg) {
-        list.appendChild(noTaskMessage);
-    } else if (list.children.length > 0 && msg) {
-        msg.remove();
+    const noTaskMessage = document.createElement("p");
+    noTaskMessage.textContent = "no tasks yet";
+    noTaskMessage.id = "no-task-message";
+    noTaskMessage.style.color = "#abb9bd";
+    
+// ------------------ Проверка сообщений ------------------
+    function updateNoTaskMessage() {
+        const msg = document.querySelector("#no-task-message");
+        if (list.children.length === 0 && !msg) {
+            list.appendChild(noTaskMessage);
+        } else if (list.children.length > 0 && msg) {
+            msg.remove();
+        }
     }
-}
 
-// проверяем при загрузке
-updateNoTaskMessage();
+    // проверяем при загрузке
+    updateNoTaskMessage();
 
-createTask.addEventListener("click", () => {
-    const taskText = input.value;
-    if (taskText === "") return;
+    input.addEventListener('keydown', function(event) {
+        if (event.key === 'Enter') {
 
-    const li = document.createElement("li");
-    li.textContent = taskText;
-
-    const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Delete";
-
-    deleteBtn.addEventListener("click", () => {
-        li.remove();
-        updateNoTaskMessage(); // проверяем после удаления
+        }
     });
 
-    li.appendChild(deleteBtn);
-    list.appendChild(li);
+    createTask.addEventListener("click", () => {
+        const taskText = input.value;
+        if (taskText === "") return;
 
-    li.style.display = "flex";
-    li.style.justifyContent = "space-between";
-    li.style.alignItems = "center";
-    deleteBtn.style.margin = "10px";
+        const li = document.createElement("li");
+        li.textContent = taskText;
 
-    input.value = "";
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "Delete";
 
-    updateNoTaskMessage(); // проверяем после добавления
-});
+        deleteBtn.addEventListener("click", () => {
+            li.remove();
+            updateNoTaskMessage(); // проверяем после удаления
+        });
+
+        li.appendChild(deleteBtn);
+        list.appendChild(li);
+
+        li.style.display = "flex";
+        li.style.justifyContent = "space-between";
+        li.style.alignItems = "center";
+        deleteBtn.style.margin = "10px";
+
+        input.value = "";
+
+        updateNoTaskMessage(); // проверяем после добавления
+    });
